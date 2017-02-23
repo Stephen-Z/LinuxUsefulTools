c912bbfae54c0f00e4d97c05ec2af13a7fadf3dc
stephen/lzm <zhangyingheng1995@hotmail.com>
2017-02-23 15:11:34 +0800

    change directory

--- aurinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "cloning from git..."
-git clone https://aur.archlinux.org/$1\.git
-echo "completed! making and install the package"
-cd $1
-makepkg -si
--- empty.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TrashLocation="/home/lzm/Desktop/Trash/*"
-echo "Emptying the Trash $TrashLocation"
-rm -rf $TrashLocation
